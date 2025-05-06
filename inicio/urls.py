059ef703c1119efdeca253f2edfa3ba57ec00918
Msskre <dbachkatov14@gmail.com>
2025-05-05 23:51:32 -0400

    Endpoint /ping/ agregado

--- inicio/urls.py
+++ inicio/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
+from django.http import JsonResponse
+
+def ping(request):
+    return JsonResponse({"status": "ok"})
 
 urlpatterns = [
+    path('ping/', ping, name='ping'),
     path('', views.menu, name='index'),
     path('galeria/', views.galeria, name='galeria'),
     path('servicios/', views.servicios, name='servicios'),
