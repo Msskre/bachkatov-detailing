5f03ffe84337ef2fddaeebe2bbe4acf0fcd10262
Dimitri Bachkatov <dbachkatov14@gmail.com>
2025-09-17 19:12:45 -0300

    Prueba de actualizaci6

--- DetailingBachkatov/settings.py
+++ DetailingBachkatov/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Seguridad básica
 # -------------------------
 SECRET_KEY = os.environ.get('SECRET_KEY', 'unsafe-default-key')
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 ALLOWED_HOSTS = ['b-detailing.onrender.com', 'localhost', '127.0.0.1']
 
 # -------------------------
@@ -26,7 +26,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',  # útil para local
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # para servir estáticos en local
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -70,10 +70,10 @@ DATABASES = {
 # Password validation
 # -------------------------
 AUTH_PASSWORD_VALIDATORS = [
-    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',},
-    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',},
-    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',},
-    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',},
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
 
 # -------------------------
@@ -87,8 +87,8 @@ USE_TZ = True
 # -------------------------
 # STATIC & MEDIA FILES
 # -------------------------
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # donde collectstatic pone los archivos
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'inicio/static')]  # donde están tus archivos locales
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # destino de collectstatic
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'inicio', 'static')]  # tus archivos locales
 
 GS_BUCKET_NAME = os.environ.get('GS_BUCKET_NAME', 'archivos-static')
 
@@ -99,21 +99,26 @@ if os.path.exists(os.path.join(BASE_DIR, 'key.json')):
     )
 elif os.environ.get('GS_CREDENTIALS'):
     key_dict = json.loads(os.environ['GS_CREDENTIALS'])
-    # Reemplaza los \n dobles por saltos de línea reales
     key_dict['private_key'] = key_dict['private_key'].replace('\\n', '\n')
     GS_CREDENTIALS = service_account.Credentials.from_service_account_info(key_dict)
 else:
     GS_CREDENTIALS = None
 
-# Configuración según entorno
-if not DEBUG or os.environ.get('FORCE_GCS') == 'True':
+# Decidir almacenamiento según entorno
+if not DEBUG or os.environ.get('FORCE_GCS', '').lower() == 'true':
+    # Producción o forzado a GCS
     STATICFILES_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
     DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
     GS_DEFAULT_ACL = 'publicRead'
     STATIC_URL = f'https://storage.googleapis.com/{GS_BUCKET_NAME}/'
+    MEDIA_URL = f'https://storage.googleapis.com/{GS_BUCKET_NAME}/media/'
 else:
+    # Local con WhiteNoise
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
     STATIC_URL = '/static/'
+    MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
